afc8c35e5cbdefc5c1c7a2d162a6b3f18b68db4d
Keen Ballon <keenballon@gmail.com>
2021-05-08 23:38:17 +0800

    Added more custom errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
    err: Error,
@@ -6,7 +7,9 @@ export const errorHandler = (
    res: Response,
    next: NextFunction
 ) => {
-   res.status(400).send({
-      message: err.message,
-   });
+   if (err instanceof CustomError) {
+      return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+   }
+
+   res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
