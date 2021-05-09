ec133f74c4f7278e530c202aee69ccadf790c7a9
Keen Ballon <keenballon@gmail.com>
2021-05-09 23:30:07 +0800

    added errs, models, and infra code

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/currentuser";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,9 +16,27 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signUpRouter);
 app.use(signOutRouter);
+app.all("*", async (req, res) => {
+   throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () => {
+   try {
+      await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+      });
+      console.log("Connected to MongoDB");
+   } catch (err) {
+      console.log(err);
+   }
+
+   app.listen(3000, () => {
+      console.log("Listening on port 3000!!!!!!!!");
+   });
+};
+
+start();
