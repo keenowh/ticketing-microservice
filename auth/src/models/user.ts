89efc217780f3c6614523e272c3bd7c4652d94f7
Keen Ballon <keenballon@gmail.com>
2021-05-14 21:02:47 +0800

    completed error handlers

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,7 +1,20 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 //An interface that describes properties that are required to create a new user
 interface UserAttrs {
+   email: string;
+   password: string;
+}
+
+// interface that decscibes the properties
+// that User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+   build(attrs: UserAttrs): UserDoc;
+}
+
+// Interface properties that a User Document has
+interface UserDoc extends mongoose.Document {
    email: string;
    password: string;
 }
@@ -17,10 +30,19 @@ const userSchema = new mongoose.Schema({
    },
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (done) {
+   if (this.isModified("password")) {
+      const hashed = await Password.toHash(this.get("password"));
+      this.set("password", hashed);
+   }
 
-const buildUser = (attrs: UserAttrs) => {
+   done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
    return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/bad-request-error";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -21,9 +22,16 @@ router.get(
          throw new RequestValidationError(errors.array());
       }
       const { email, password } = req.body;
+      const existingUser = await User.findOne({ email });
 
-      console.log("Creating a user...");
-      throw new DatabaseConnectionError();
+      if (existingUser) {
+         throw new BadRequestError("Email in use");
+      }
+
+      const user = User.build({ email, password });
+      await user.save();
+
+      res.status(201).send(user);
    }
 );
 
