621cf26ff54b5cb4dbf19af446f9add6a18cb8e7
Keen Ballon <keenballon@gmail.com>
2021-06-20 23:39:52 +0800

    added more updates

---
+++ client/hooks/use-request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+
+const [errors, setErrors] = useState(null);
+export default ({ url, method, body, onSuccess }) => {
+   const doRequest = async () => {
+      try {
+         const response = await axios[method](url, body);
+
+         if (onSuccess) {
+            onSuccess(response.data);
+         }
+         return response.data;
+      } catch (err) {
+         setErrors(
+            <div className="alert alert-danger">
+               <h4></h4>
+               <ul className="my-0">
+                  {err.response.data.errors.map((err) => {
+                     <li>{err.message}</li>;
+                  })}
+               </ul>
+            </div>
+         );
+      }
+   };
+
+   return { doRequest, errors };
+};
---
+++ client/pages/auth/signup.js
@@ -0,0 +1,50 @@
+import Router from "next/router";
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+
+export default () => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [doRequest, errors] = useRequest({
+      url: "/api/users/signup",
+      method: "post",
+      body: {
+         email,
+         password,
+      },
+      onSuccess: () => {
+         Router.push("/");
+      },
+   });
+   const onSubmit = async (event) => {
+      event.preventDefault();
+
+      await doRequest();
+   };
+
+   return (
+      <form onSubmit={onSubmit}>
+         <h1>Sign Up</h1>
+         <div className="form-group">
+            <label htmlFor="">Email Address</label>
+            <input
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+               className="form-control"
+               type="email"
+            />
+         </div>
+         <div className="form-group">
+            <label htmlFor="">Password</label>
+            <input
+               className="form-control"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
+               type="password"
+            />
+         </div>
+         {errors}
+         <button className="btn btn-primary">Sign Up</button>
+      </form>
+   );
+};
