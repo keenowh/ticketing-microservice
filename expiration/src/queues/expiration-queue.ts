221ebe1c8202053fa4a0ad022b1be607ae368575
Keen <keenballon@gmail.com>
2021-08-20 23:47:58 +0800

    updates on events

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,13 @@
+import Queue from "bull";
+
+interface Payload {
+    orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>("order:expiration", {
+    redis: { host: process.env.REDIS_HOST },
+});
+
+expirationQueue.process(async (job) => {});
+
+export { expirationQueue };
